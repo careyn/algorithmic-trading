b64cc88e8ac03d0a5bec9b086a543b45916a16a3
careyn <carey.n@northeastern.edu>
2021-09-10 01:10:48 -0400

    Adding initial trading functions

--- algo_trader.py
+++ algo_trader.py
@@ -13,7 +13,7 @@ def connect(login, password):
         print("Connected: Connecting to MT5 Client")
     else:
         print("Failed to connect at account #{}, error code: {}"
-              .format(account, mt5.last_error()))
+              .format(login, mt5.last_error()))
 
 # Testing rate collection
 # utc_from = datetime(2021, 9, 1)
@@ -36,6 +36,45 @@ def open_position(pair, order_type, size, tp_distance=None, stop_distance=None):
             print("symbol_select({}}) failed, exit",pair)
             return
     print(pair, "found!")
+
+    point = symbol_info.point
+    
+    if(order_type == "BUY"):
+        order = mt5.ORDER_TYPE_BUY
+        price = mt5.symbol_info_tick(pair).ask
+        if(stop_distance):
+            sl = price - (stop_distance * point)
+        if(tp_distance):
+            tp = price + (tp_distance * point)
+            
+    if(order_type == "SELL"):
+        order = mt5.ORDER_TYPE_SELL
+        price = mt5.symbol_info_tick(pair).bid
+        if(stop_distance):
+            sl = price + (stop_distance * point)
+        if(tp_distance):
+            tp = price - (tp_distance * point)
+
+    request = {
+        "action": mt5.TRADE_ACTION_DEAL,
+        "symbol": pair,
+        "volume": float(size),
+        "type": order,
+        "price": price,
+        "sl": sl,
+        "tp": tp,
+        "magic": 234000,
+        "comment": "",
+        "type_time": mt5.ORDER_TIME_GTC,
+        "type_filling": mt5.ORDER_FILLING_IOC,
+    }
+
+    result = mt5.order_send(request)
+
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        print("Failed to send order :(")
+    else:
+        print ("Order successfully placed!")
 
 # Testing calls
 # connect(login, password)
